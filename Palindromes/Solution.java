6ae7c1fd79fa89152a3cd7725e91a70e6cb45190
Arivoli AE <aayakarambla@vmware.com>
2019-08-06 13:09:49 -0700

    Commit for Backtracking 2 - Palindrome Permutation

---
+++ Palindromes/Solution.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class PalindromePartition {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        int position = 0;
+        helper(result, new ArrayList<>(), s, position);
+        return result;
+    }
+
+    public void helper(List<List<String>> result, List<String> tempList, String s, int position){
+        if(position == s.length()){
+            result.add(new ArrayList<>(tempList));
+        }
+        else {
+            for(int i=position;i<s.length();i++){
+                if(isPalindrome(s, position, i)){
+                    tempList.add(s.substring(position, i+1));
+                    helper(result, tempList, s, i+1);
+                    tempList.remove(tempList.size() - 1);
+                }
+            }
+        }
+    }
+
+    public boolean isPalindrome(String s, int start, int end){
+        while(start < end){
+            if(s.charAt(start) != s.charAt(end))    return false;
+            start++;end--;
+        }
+
+        return true;
+    }
+}
+
+class Solution {
+    public static void main(String[] args){
+        System.out.println("PalindromePartition");
+        PalindromePartition obj = new PalindromePartition();
+        System.out.println(obj.partition("aab"));
+    }
+}
