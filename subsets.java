d929eb4a0d797dd76adb51373dfaac4faafb6a96
Mahima Gupta <mahimagupta1195@gmail.com>
2019-08-06 20:28:23 -0700

    Cooments

--- subsets.java
+++ subsets.java
@@ -1,14 +1,12 @@
-// Time Complexity :O(n2)
-// Space Complexity :
-// Did this code successfully run on Leetcode :Yes
-// Any problem you faced while coding this :Understanding the solution
+//famous problem at facebook
+//Time complexity:n*(2raise to n) 
 
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
       List<List<Integer>> result = new ArrayList();
       result.add(new ArrayList());
       for(int n:nums){
-          int size = result.size();
+          int size = result.size(); //size of the list is 2raise to n(that many combinations)
           for(int i=0;i<size;i++){
               List<Integer> sublist = new ArrayList(result.get(i));
               sublist.add(n);
