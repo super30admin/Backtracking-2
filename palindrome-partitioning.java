3706cd3c8d031d04d15bcea441e02a2fd8afe20d
Pooja Ramaswamy <poojaramaswamy94@gmail.com>
2019-09-24 17:01:36 -0700

    Completed PR Backtracking 2

---
+++ palindrome-partitioning.java
@@ -0,0 +1,35 @@
+//Time complexity: O(n*2^n)
+//Space complexity: O(N)
+//Attempted on leetcode and accepted
+class Solution {
+    public List<List<String>> partition(String s) 
+    {
+       List<List<String>> list = new ArrayList<>();
+       backtrack(list, new ArrayList<>(), s, 0);
+       return list;
+    }
+
+public void backtrack(List<List<String>> list, List<String> tempList, String s, int start)
+    {
+       if(start == s.length())
+          list.add(new ArrayList<>(tempList));
+       else
+       {
+          for(int i = start; i < s.length(); i++)
+          {
+            if(isPalindrome(s, start, i))
+            {
+                tempList.add(s.substring(start, i + 1));
+                backtrack(list, tempList, s, i + 1);
+                tempList.remove(tempList.size() - 1);
+            }
+          }
+       }
+    }
+
+    public boolean isPalindrome(String s, int low, int high){
+       while(low < high)
+          if(s.charAt(low++) != s.charAt(high--)) return false;
+       return true;
+    }
+}
