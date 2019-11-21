ac6c56d194615a29116023a74ee528334b395827
Michael Mateo <michaelmateoleiton@gmail.com>
2019-11-20 21:42:46 -0600

    Fixed solution

--- PalindromePartitioning.java
+++ PalindromePartitioning.java
@@ -1,44 +1,47 @@
 /**
- * [WIP]
- * Leetcode: No running
+ * Time Complexity: O(n^n)??
+ * Space Complexity: O(n)?
+ * Leetcode: Runs Successfully
+ * Problems: I have problems understanding time/space complexity of backtracking problems.
+ * Will be reviewing this weekend
  */
 
 class Solution {
-    List<List<String>> result;
+    private List<List<String>> result;
     public List<List<String>> partition(String s) {
         result = new ArrayList<>();
-        //backtracking function
-        backtrack(s, 0, new ArrayList<>());
+        //call getPartitions()
+        getPartitions(s, 0, new ArrayList<>());
         return result;
     }
     
-    private void backtrack(String s, int index, List<String> substrings){
-        if(index >= s.length() && substrings.size() > 0){
-            result.add(new ArrayList<>(substrings));
+    private void getPartitions(String s, int index, List<String> list){
+        //base case: if we have gone past the length of our string, add list to result, then return
+        if(index == s.length()){
+            result.add(new ArrayList<>(list));
             return;
-        } 
-         
+        }
+        
         for(int i = index; i < s.length(); i++){
-            String substring = s.substring(index, i);
-            if(isPalindrome(substring)) {
-                if(index == i) substrings.add(Character.toString(s.charAt(i)));
-                else substrings.add(s.substring(index, i + 1));
-                backtrack(s, i + 1, substrings);
-                substrings.remove(substrings.size() - 1);
+            String sub = s.substring(index, i + 1);
+            if(isPalindrome(sub)){
+                list.add(sub);
+                getPartitions(s, i+1, list);
+                list.remove(list.size() - 1);
             }
         }
     }
     
     private boolean isPalindrome(String s){
-        int right = s.length() - 1;
+        if(s.length() == 0) return true;
         int left = 0;
+        int right = s.length() - 1;
         
         while(left < right){
             if(s.charAt(left) != s.charAt(right)) return false;
             left++;
             right--;
         }
-        
         return true;
     }
 }
