ec191b085b725c1fd4b08a67f2c895d9de040622
Sweety Sojrani <sweety.sojrani@sjsu.edu>
2020-01-18 16:05:08 -0800

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -1,5 +1,5 @@
-// Time Complexity
-// Space Complexity
+// Time Complexity - O(2^n)
+// Space Complexity - O(1)
 // This Solution worked on LeetCode
 
 
@@ -14,14 +14,15 @@ class Solution {
     private void backtrack(String s, int start,List<String> temp){
         //Base Case
         if(start == s.length()){
-            result.add(new ArrayList<>(temp));
+            result.add(new ArrayList<>(temp));  // if end of String is reached and it means the  palindromes are obtained in the temp.
             return;
         }
         // Logic
         for(int i=start ;i < s.length(); i++){
             if(ispalindrome(s,start,i)){
-                temp.add(s.substring(start,i+1));
-                backtrack(s,i+1,temp);
+                temp.add(s.substring(start,i+1));   // Add to the temp list if palindrome
+                backtrack(s,i+1,temp);          // recursively call for the next characters in the string
+                // Backtrack to get other paths from the previous character
                 temp.remove(temp.size() - 1);
             }
         }
