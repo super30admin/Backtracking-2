9ec7b1ba9f2236fdce62d2c64dc3eea9e8e81f8c
Shashank Baghel <41822011+Shashank001122@users.noreply.github.com>
2020-07-01 05:46:07 -0700

    Update Subsets.py

--- Subsets.py
+++ Subsets.py
@@ -20,4 +20,27 @@ class Solution:
         
         
  time complexity is O(2 raise to n)
- Space comlexity is O(n*2 raise to n)
+ Space comlexity is O(n*(2 raise to n))
+    ===========================================================================
+    
+import copy
+class Solution:
+    def __init__(self):
+        self.result=[]
+        
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+        if nums==None or len(nums)==0:
+            return self.result
+        self.helper(nums,[],0)
+        return self.result
+    def helper(self,nums,temp,i):
+        if i==len(nums):
+            self.result.append(temp)
+            return 
+        #choose
+        self.helper(nums,temp+[nums[i]],i+1)
+        #not choose
+        self.helper(nums,temp,i+1)
+time complexity is O(2 raise to n)
+Space comlexity is O(n*(2 raise to n))
