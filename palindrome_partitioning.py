d8644f833e51481daba39246ef0efd9b90b25cc7
Mohan Reddy 1544 <54518017+Mohan110594@users.noreply.github.com>
2020-08-12 08:52:04 -0600

    Update palindrome_partitioning.py

--- palindrome_partitioning.py
+++ palindrome_partitioning.py
@@ -1,4 +1,4 @@
-// Time Complexity :O(n^2)
+// Time Complexity :O(n*(2^n))
 // Space Complexity :O(n)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : None
