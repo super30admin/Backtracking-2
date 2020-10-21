77c2b9285af91028b69b8d2c36c210751fba2d06
alekhyagudimetla <alekhya.gudimetla9@gmail.com>
2020-10-20 20:36:31 -0500

    Create palindrome-partitioning.py

---
+++ palindrome-partitioning.py
@@ -0,0 +1,28 @@
+Time Complexity: O(2^n) - 
+Space Compplexity: O(n)
+class Solution(object):
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        def isPalindrome(start_ind, end_ind):
+            while start_ind <= end_ind:
+                if s[start_ind] != s[end_ind]: return False
+                start_ind += 1
+                end_ind -=1
+            return True
+        
+        def dfs(start_ind, path):
+            print(start_ind)
+            print(path)
+            if start_ind >= len(s): self.res.append(path)
+                
+            for l in range(len(s) - start_ind):
+                print(l)
+                if isPalindrome(start_ind, start_ind + l):
+                    dfs(start_ind + l + 1, path + [s[start_ind:start_ind + l + 1]] )
+        
+        self.res = []
+        dfs(0,[])
+        return self.res
