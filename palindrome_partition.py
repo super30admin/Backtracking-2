62eebe55e7c2d4fc276b99c51d2b73a839758ef7
Sachin Devatar <73754947+SachinDevatar77@users.noreply.github.com>
2021-01-28 15:39:12 -0600

    Create palindrome_partition.py

---
+++ palindrome_partition.py
@@ -0,0 +1,44 @@
+# Time Complexity : O(N * 2^N) O(N) - checking palindrome ability and O(2^N) since we have a two choice in recursive tree
+# Space Complexity : O(N) Take space in backtracking
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+class Solution(object):
+    def partition(self, s):
+        self.result = []
+        
+        if not s or len(s) == "":
+            return self.result
+        
+        self.backtrack(s, 0, [])
+        return self.result
+    
+    def backtrack(self, string, index, path):
+        #Base
+        if index == len(string):
+            self.result.append(list(path))
+            return 
+        
+        #Logic 
+        for i in range(index, len(string)): # not from zero, from index(that is index)
+            if self.checkPalindrome(string, index, i):        
+                #action
+                path.append(string[index: i+1]) # in slicing i+1 is not added
+                #recurse
+                self.backtrack(string, i+1, path)
+                #backtrack
+                path.pop()
+
+    
+    # Palindrome check 
+    def checkPalindrome(self, string, left, right):
+        if left == right:
+            return True
+        while left < right:
+            if string[left] != string[right]:
+                return False
+            
+            left += 1
+            right -= 1
+            
+        return True
