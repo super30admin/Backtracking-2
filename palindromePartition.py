c91d91f1f2d34d96247809b76a5d87c99baa1842
amby2964 <75859704+amby2964@users.noreply.github.com>
2021-01-28 01:20:38 -0700

    Create palindromePartition.py

---
+++ palindromePartition.py
@@ -0,0 +1,19 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        
+        res = []
+        
+        def part(index,path):
+            nonlocal res
+            if(index==len(s)):
+                res.append(path[:])
+                return
+            for i in range(index,len(s)):
+                if(s[index:i+1]== s[index:i+1][::-1]):
+                    path.append(s[index:i+1])
+                    part(i+1,path)
+                    path.pop()
+        
+        
+        part(0,[])
+        return res
