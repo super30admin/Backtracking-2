eeecbeb3ccda2e64c5aa02f2c36a8e206a7f017e
Durgesh Patil <patildurgesh1998@gmail.com>
2021-08-27 14:16:44 -0400

    Update Problem-1.cpp

--- Problem-1.cpp
+++ Problem-1.cpp
@@ -120,3 +120,27 @@ public:
         }
     }
 };
+
+/*
+Time Complexity = Exponential
+Space Complexity = Exponential
+*/
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int>temp;
+        result.push_back(temp);
+        for(int num : nums)
+        {
+            int size = result.size();
+            for(int i=0;i<size;i++)
+            {
+                vector<int>temp(result[i]);
+                temp.push_back(num);
+                result.push_back(temp);
+            }
+        }
+        return result;
+    }
+};
