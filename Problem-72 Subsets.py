46efcbf526e660d9e9de1fca3e487d654b4a38d3
Adhish Shrivastava <adhish70@gmail.com>
2021-11-15 12:44:41 -0500

    Initial Commit

---
+++ Problem-72 Subsets.py
@@ -0,0 +1,11 @@
+# 78. Subsets
+# https://leetcode.com/problems/subsets/
+
+# Logic: 
+
+# Time Complexiety: 
+# Space Complexiety: 
+
+class Solution:  
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        pass
---
+++ Problem-73 Palindrome Partitioning.py
@@ -0,0 +1,11 @@
+# 131. Palindrome Partitioning
+# https://leetcode.com/problems/palindrome-partitioning/
+
+# Logic: 
+
+# Time Complexiety: 
+# Space Complexiety: 
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        pass
