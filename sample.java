044d1c219e6040f9168f35bb9305b3eeaeb5e710
Ikshaku Goswami <ig752994@gmail.com>
2022-01-05 15:34:21 +0530

    completed backtracking2

--- sample.java
+++ sample.java
@@ -1,3 +1,43 @@
+//palindrome partitioning
+// Time Complexity : O(2^n)
+// Space Complexity : O(n)
+class Solution {
+    List<List<String>> result;
+    public List<List<String>> partition(String s) {
+        result = new ArrayList<>();
+        helper(s, 0, new ArrayList<String>());
+        return result;
+    }
+    private void helper(String s, int index, List<String> path){
+        
+        if(index==s.length()){
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        
+        for(int i = index; i<s.length(); i++){
+            String curr = s.substring(index, i+1);
+            if(isPalindrome(curr)){
+                path.add(curr);  //action
+                helper(s, i+1, path);   //recurse
+                path.remove(path.size()-1); //backtrack
+            }
+        }
+        
+    }
+    private boolean isPalindrome(String s){
+        int left = 0;
+        int right = s.length()-1;
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
 //subsets
 // Time Complexity : O(n*2^n)
 // Space Complexity : O(n)
