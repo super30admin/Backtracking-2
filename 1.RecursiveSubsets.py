d96f3f86985e122e3dffcfd10e26d74b6f3702d8
pavanghuge <pbg7@njit.edu>
2022-01-31 08:47:43 -0500

    Backtracking-2 Completed

---
+++ 1.RecursiveSubsets.py
@@ -0,0 +1,26 @@
+# Recursive Approach without dictionary
+# // Time Complexity : O(2^N)
+# // Space Complexity : O(N)
+# // Did this code successfully run on Leetcode :Yes
+# // Any problem you faced while coding this :
+class Solution:
+    result = list()
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        self.result = []
+        self.helper(nums, 0,list())
+        return self.result
+    def helper(self,nums, index,path):
+        
+        # base
+        if index == len(nums):
+            self.result.append(path)
+            return 
+        
+        # logic
+        # not choose
+        self.helper(nums, index+1, path+list())
+        
+        # choose
+        temp = path
+        temp.append(nums[index])
+        self.helper(nums, index+1, temp)
