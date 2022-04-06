f293e941dd221b72dd0b0168d14dff3ee36fc8d1
architakale <architakale@knights.ucf.edu>
2022-04-06 00:24:25 -0400

    complete backtracking 2

---
+++ Problem1.py
@@ -0,0 +1,28 @@
+# Time Complexity : O(N * 2^N)
+# Space Complexity : O(N)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        def backtrack(first = 0, curr = []):
+            # if the combination is done
+            if len(curr) == k:  
+                output.append(curr[:])
+                return
+            for i in range(first, n):
+                # add nums[i] into the current combination
+                curr.append(nums[i])
+                # use next integers to complete the combination
+                backtrack(i + 1, curr)
+                # backtrack
+                curr.pop()
+        
+        output = []
+        n = len(nums)
+        for k in range(n + 1):
+            backtrack()
+        return output
