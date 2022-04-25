f98769087854f3e9e089636a1f0e554597e8d662
BitLegion1996 <93818475+BitLegion1996@users.noreply.github.com>
2022-04-25 00:59:43 -0700

    Create subsets.py

---
+++ subsets.py
@@ -0,0 +1,25 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+        '''
+        T = O( N*(2^N) )
+        S = O(N)
+        '''
+        
+        
+        def helper( nums, idx, r  ):
+            # print(r)
+            result.append(list(r))
+            if idx >= len(nums):
+                # result.append(list(r))
+                return
+            
+            for i in range(  idx ,   len(nums)):
+                r.append(nums[i])
+                helper( nums, i+1 , r )
+                r.pop()
+                
+            
+        result = [ ]
+        helper( nums, 0, [] )
+        return result
