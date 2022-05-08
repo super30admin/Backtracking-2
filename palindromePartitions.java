7745aca79625793335092842014408b0e45f50d2
Vaishnavi Kannan <v.kannan@salesforce.com>
2022-05-08 16:54:32 -0700

    Backtracking 2

--- palindromePartitions.java
+++ palindromePartitions.java
@@ -44,3 +44,35 @@ class Solution {
         return true;
     }
 }
+
+// DP for maintaining whether substring is a palindrome
+class Solution {
+    List<List<String>> palindromePartitions = null;
+    public List<List<String>> partition(String s) {
+        palindromePartitions = new ArrayList<>();
+        boolean dp[][] = new boolean[s.length()][s.length()];
+        backtrack(s, 0, new ArrayList<>(), dp);
+        return palindromePartitions;
+    }
+    
+    private void backtrack(String s, int index, List<String> curPalindromes, boolean[][] dp) {
+        // Base
+        if (index == s.length()) {
+            palindromePartitions.add(new ArrayList<>(curPalindromes));
+            return;
+        }
+        
+        // Logic
+        for (int i = index; i < s.length(); ++i) {
+            // 0 1 2 3 4 
+            // a a a b a
+            // index = 0 and i = 4, we want to check if the string from index 1 - 3 is a palindrome too
+            if (s.charAt(index) == s.charAt(i) && (i - index <= 2 || dp[index+1][i-1])) {
+                dp[index][i] = true;
+                curPalindromes.add(s.substring(index, i + 1));
+                backtrack(s, i + 1, curPalindromes, dp);
+                curPalindromes.remove(curPalindromes.size() - 1);
+            }
+        }
+    }
+}
