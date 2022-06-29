855ecfafb43f17e72a82a7b6e5481fa9f15e93c5
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-06-29 12:37:09 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -67,3 +67,29 @@ class Solution {
         
     }
 }
+
+//****SUBSETS - ITERATIVE TECHNIQUE****
+//Time complexity: 2 res to n
+//Space complexity:n;
+//Leetcode runnable: Y;
+//Any doubts:N;
+class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> subsets(int[] nums) {
+        result=new ArrayList<>();
+        result.add(new ArrayList<>());
+        for(int i=0;i<nums.length;i++)
+        {
+            int size=result.size();
+            for(int j=0;j<size;j++)
+            {
+                List<Integer> temp=new ArrayList<>(result.get(j));
+                temp.add(nums[i]);
+                result.add(temp);
+            }
+        }
+        return result;
+        
+    }
+    
+}
