91e63afae85f1f7b66d854242c30174b63490de8
Rahil Shah <rahilshah@Rahils-MacBook-Air.local>
2022-07-29 10:51:22 -0700

    Completed Backtracking-2

---
+++ PalindromePartitioning.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//Time Complexity- Exponential
+//Space Complexity- O(n)
+//Sucessfully ran on leetcode
+
+class Solution {
+ List<List<String>> result;
+ public List<List<String>> partition(String s) {
+     
+     result= new ArrayList<>();
+     if(s==null || s.length() ==0) return result;
+     
+     helper(s, 0, new ArrayList<>());
+     return result;
+ }
+ private void helper(String s, int index, List<String> path){
+     
+     //base
+     if(index == s.length()){
+         result.add(new ArrayList(path));
+         return;
+     }
+     //logic
+     for(int i= index; i< s.length(); i++){
+         if(isPalindrome(s,index,i)){
+             
+             //action
+             path.add(s.substring(index,i+1));
+             
+             //recurse
+             helper(s, i+1, path);
+             
+             //backtrack
+             path.remove(path.size() - 1);
+             
+         }
+         
+     }
+ }
+ 
+ private boolean isPalindrome(String s , int left, int right){
+     while(left<= right){
+         if(s.charAt(left)!= s.charAt(right)) return false;
+         left++;
+         right--;
+     }
+     return true;
+ }
+}
