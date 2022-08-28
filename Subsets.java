839ac6d4ccedb6e686ada9db25e8fe4a8e1c6bbc
Jay Tanna <31876791+jaytanna61@users.noreply.github.com>
2022-08-27 21:05:06 -0400

    Create Subsets.java

---
+++ Subsets.java
@@ -0,0 +1,52 @@
+/*class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> subsets(int[] nums) {
+        result = new ArrayList<List<Integer>>();
+        helper(nums,0,new ArrayList<>());
+        return result;
+    }
+    private void helper(int[] nums,int idx, List<Integer> path){
+        //base
+        if(idx >= nums.length){
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        helper(nums,idx+1,path);
+        path.add(nums[idx]);
+        helper(nums,idx+1,path);
+        path.remove(path.size()-1);
+    }
+}*/
+class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> subsets(int[] nums) {
+        result = new ArrayList<List<Integer>>();
+        result.add(new ArrayList<>());
+        helper(nums,0,new ArrayList<>());
+        return result;
+    }
+    private void helper(int[] nums,int idx, List<Integer> path){
+        for(int i = idx;i<nums.length;i++){
+            path.add(nums[i]);
+            result.add(new ArrayList<>(path));
+            helper(nums,i+1,path);
+            path.remove(path.size()-1);
+        }
+    }
+}
+
+/*class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        result.add(new ArrayList<>());
+        for(int i = 0;i<nums.length;i++){
+            int size =result.size();
+            for(int j = 0;j<size;j++){
+                List<Integer> temp = new ArrayList<>(result.get(j));
+                temp.add(nums[i]);
+                result.add(temp);
+            }
+        }
+        return result;
+    }
+}*/
