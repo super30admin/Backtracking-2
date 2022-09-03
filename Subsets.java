35b7ca55920a72235f93a0d9a305ebfe95f30efc
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2022-09-02 23:56:14 -0700

    Subsets using iteartive approach

--- Subsets.java
+++ Subsets.java
@@ -1,8 +1,8 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-// Time Complexity : O(2 power n) where n = number of nodes
-// Space Complexity : O(h) where h = height of tree
+// Time Complexity : O(2 power n) where n = number of elements in array
+// Space Complexity : O(1)
 // Did this code successfully run on Leetcode :
 // Any problem you faced while coding this :
 
@@ -10,27 +10,44 @@ import java.util.Queue;
 //78. Subsets (Medium) - https://leetcode.com/problems/subsets/
 class Solution {
     
-    List<List<Integer>> result;
+    //List<List<Integer>> result;
     
     public List<List<Integer>> subsets(int[] nums) {
-        result = new ArrayList<>();
-        helper(nums, 0, new ArrayList<>());
-        return result;
-    }
-    
-    private void helper(int[] nums, int pivot, List<Integer> path) {
-        // base
-        // logic
-        result.add(new ArrayList<>(path));
-        for (int i = pivot; i < nums.length; i++) {
-            // action
-            path.add(nums[i]);
+    	// recursive approach
+//        result = new ArrayList<>();
+//        helper(nums, 0, new ArrayList<>());
+//        return result;
+//    }
+//    
+//    private void helper(int[] nums, int pivot, List<Integer> path) {
+//        // base
+//        // logic
+//        result.add(new ArrayList<>(path));
+//        for (int i = pivot; i < nums.length; i++) {
+//            // action
+//            path.add(nums[i]);
+//            
+//            // recurse
+//            helper(nums, i+1, path);
+//            
+//            // backtrack
+//            path.remove(path.size()-1);
+//        }
+    	
+    	// iterative approach
+    	List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        
+        for (int i = 0; i < nums.length; i++) {
+            int size = result.size();
             
-            // recurse
-            helper(nums, i+1, path);
-            
-            // backtrack
-            path.remove(path.size()-1);
+            for (int j = 0; j < size; j++) {
+                List<Integer> temp = new ArrayList<>(result.get(j));
+                temp.add(nums[i]);
+                result.add(temp);
+            }
         }
+        
+        return result;
     }
 }
