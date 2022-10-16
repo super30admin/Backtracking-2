b1bb26a9023f8537f928d207be9d7fe9c2d972bf
Vedant Nibandhe <vnibandhe@gmail.com>
2022-10-15 19:47:34 -0500

    Completed Sprint Backtracking-2

---
+++ subsets.py
@@ -0,0 +1,17 @@
+#Time Complexity: O(N*2^N)
+#Space Complexity: O(N*2^N)
+#Did it run on leetcode: Yes
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        output = []
+        
+        for i in range(2**n, 2**(n + 1)):
+            # generate bitmask, from 0..00 to 1..11
+            bitmask = bin(i)[3:]
+            
+            # append subset corresponding to that bitmask
+            output.append([nums[j] for j in range(n) if bitmask[j] == '1'])
+        
+        return output
