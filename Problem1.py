080c59015017ce5227d1583d27b424f17cdd4140
Niyati Bavishi <niyatibavishi@Niyatis-MacBook-Pro.local>
2022-11-10 18:41:07 -0500

    Bactracking-2

--- Problem1.py
+++ Problem1.py
@@ -1,22 +1,21 @@
-#Time Complexity - O(2^n)
+#Time complexity - O(2^n)
 #Space Complexity - O(n)
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
-        def explore( index, target, arr):
-            nonlocal result
-            if target == 0:
-                result.append(arr.copy())
-                return
-            if index >= len(candidates) or target < 0:
-                return
-            for i in range(index, len(candidates)):
-                target -=  candidates[i]
-                arr.append(candidates[i])
-                explore(i, target, arr)
-                target += arr.pop()
-            
-        explore(0, target, [])
-        return result
 
-    
+        def explore(index, arr):
+            nonlocal result 
+            result.append(arr.copy())
+
+            if index == len(nums):
+                return
+
+            for i in range(index, len(nums)):
+                arr.append(nums[i])
+                explore(i + 1, arr)
+                arr.pop()
+        
+        explore(0, [])
+        return result 
+            
