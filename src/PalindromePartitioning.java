50af8f5d4c10091b301a9e08ebc897a2f16ce1bc
Sagar Raval <62638370+SRaval20@users.noreply.github.com>
2023-06-14 19:15:42 -0700

    Update PalindromePartitioning.java

--- src/PalindromePartitioning.java
+++ src/PalindromePartitioning.java
@@ -4,52 +4,112 @@
 // Yes, this code ran successfully
 // No, I didn't face any problem in this problem statement
 
-import java.util.ArrayList;
-import java.util.List;
+// ****************************** for loop backtrack with index parameter ******************************
 
-public class PalindromePartitioning {
-    public static void main(String[] args) {
-        SolutionPalindromePartitioning obj = new SolutionPalindromePartitioning();
-        System.out.println(obj.partition("aab"));
-    }
-}
-
-class SolutionPalindromePartitioning {
+class Solution {
 
     List<List<String>> result;
 
     public List<List<String>> partition(String s) {
+        
         result = new ArrayList<>();
-        partition(s, 0, new ArrayList<>());
+        helper(s, 0, new ArrayList<>());
         return result;
+
     }
 
-    private void partition(String s, int index, List<String> list) {
+    private void helper(String s, int index, List<String> path) {
+
+        // base
         if(index == s.length()) {
-            result.add(new ArrayList<>(list));
+            result.add(new ArrayList<>(path));
+            return;
         }
 
-        for(int i = index; i < s.length(); i++) {
-            String sub = s.substring(index, i+1);
-            if(isPalindrome(sub)) {
-                // ACTION
-                list.add(sub);
-                // RECURSION
-                partition(s, i+1, list);
-                // BACKTRACKING
-                list.remove(list.size()-1);
+        // for loop
+        for(int i=index; i<s.length(); i++) {                        // for string "aaba"
+
+            String sub = s.substring(index, i+1);                    // getting substrings "a", "aa", "aab" & "aaba" 
+
+            if(isPalindrome(sub)) {                                  // if it is palindrome
+                path.add(sub);                                       // add it in path
+                helper(s, i+1, path);                                // recursive call for following characters with following index
+                path.remove(path.size()-1);                          // backtrack
             }
+
         }
+
     }
 
     private boolean isPalindrome(String s) {
-        int start = 0, end = s.length()-1;
+
+        int start = 0;
+        int end = s.length()-1;
+
         while(start < end) {
-            if(s.charAt(start) != s.charAt(end))
+            if(s.charAt(start) != s.charAt(end)) 
                 return false;
             start++;
             end--;
         }
         return true;
+
+    }
+}
+
+
+
+
+
+
+// ****************************** for loop backtrack without index parameter ******************************
+
+
+// Time Complexity:  O(n * 2^n)
+// Space Complexity: O(n)
+
+class Solution {
+
+    List<List<String>> result;
+
+    public List<List<String>> partition(String s) {
+        
+        result = new ArrayList<>();
+        helper(s, new ArrayList<>());
+        return result;
+
+    }
+
+    private void helper(String s, List<String> path) {
+
+        if(s.length() == 0) {                                   // reached end of the string
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        
+        for(int i=0; i<s.length(); i++) {                       // for string "aab"
+            String sub = s.substring(0, i+1);                   // getting "a", "aa" & "aab"
+            if(isPalindrome(sub)) {                             // if they are palindrome
+                path.add(sub);                                  // then add in path
+                helper(s.substring(i+1), path);                 // recursive for "ab", "b" & "".
+                path.remove(path.size()-1);                     // remove from path
+            }
+        }
+
+    }
+
+    private boolean isPalindrome(String s) {
+
+        int start = 0;
+        int end = s.length()-1;
+
+        while(start < end) {
+            if(s.charAt(start) != s.charAt(end)) 
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+
     }
 }
