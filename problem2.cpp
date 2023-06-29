099bef26e7b92c4f9d16f2fe90a57e90e1fd1b4c
jackpraveenraj <jackpraveenraj.work@gmail.com>
2023-06-28 17:21:54 -0700

    solved

---
+++ problem2.cpp
@@ -0,0 +1,39 @@
+// Time Complexity : O(n*(2^n))
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> result;
+        vector<string> path;
+        partition(s, 0, path, result);
+        return result;
+    }
+private: 
+
+    void partition(string& s, int start, vector<string>& path, vector<vector<string>>& result) {
+        int n = s.length();
+        if (start == n) {
+            result.push_back(path);
+        } else {
+            for (int i = start; i < n; i++) {
+                if (isPalindrome(s, start, i)) {
+                    path.push_back(s.substr(start, i - start + 1));
+                    partition(s, i + 1, path, result);
+                    path.pop_back();
+                }
+            }
+        }
+    }
+
+    bool isPalindrome(string& s, int l, int r) {
+        while (l < r) {
+            if (s[l++] != s[r--]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
