648ac329cf77b72432a2d132a9c8a21d4b8d713d
Debdyuti Das <debdyuti@Debdyutis-MacBook-Air.local>
2023-07-05 01:21:59 -0700

    Completed backtracking2

---
+++ Problem2.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    List<List<String>> result;
+
+    public List<List<String>> partition(String s) {
+        result = new ArrayList<>();
+        helper(s, 0, new ArrayList<String>());
+        return result;
+    }
+
+    public void helper(String s, int pivot, ArrayList<String> path) {
+        if (pivot == s.length()) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = pivot; i < s.length(); i++) {
+            String sub = s.substring(pivot, i + 1); 
+            if (isPalindrome(sub)) {
+                path.add(sub);
+                helper(s, i + 1, path);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    public boolean isPalindrome(String sub) {
+        int start = 0, end = sub.length() - 1;
+        while (start < end) {
+            char s = sub.charAt(start); 
+            char e = sub.charAt(end); 
+
+            if (s != e)
+                return false;
+
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
