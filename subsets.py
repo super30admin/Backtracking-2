1a53312b49d0deab01f987e8ad8c1484bda3389e
Piyush Gade <68443965+piyush1405@users.noreply.github.com>
2023-08-04 19:49:27 +0530

    Completed subsets problem

---
+++ subsets.py
@@ -0,0 +1,23 @@
+#Time: O(2^n)
+#Space: O(n)
+#issues faced: when candidate is appended as a list to the result, it is getting modified with the modification in the candidate
+#did the code run successfully on LC: yes
+#Approach:
+#using recursion form subsets
+#at every element u either chooose or not choose it.
+#when you choose the element and recurse, u backtrack again for the not choose case.
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        def backtrack(idx, candidate):
+            if idx == len(nums):
+                result.append(tuple(candidate))
+                return
+            candidate.append(nums[idx]) #place
+            backtrack(idx+1, candidate) #recurse
+            candidate.pop() #backtrack
+            backtrack(idx+1, candidate) #case0
+            
+        backtrack(0, [])
+        return result
