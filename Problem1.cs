79d1b78233441a48702e66767c75aa183e8b383e
smithaholla <smitha.holla03@gmail.com>
2023-11-04 23:48:46 -0500

    Update Problem1.cs

--- Problem1.cs
+++ Problem1.cs
@@ -1,6 +1,6 @@
 public class Subset
     {
-        // Time Complexity : O(2^n)
+        // Time Complexity : O(n * 2^n)
         // Space Complexity : O(n) - depth of the tree
         // Did this code successfully run on Leetcode : Yes
         // Any problem you faced while coding this : No
