6b14ad308328d7c291eef3fcaffd7570ae5c0aa8
Thaneer Malai Narayanan <p.kiruthikaa@gmail.com>
2023-12-12 20:59:55 -0800

    completed Backtracking-2

---
+++ Subsets.java
@@ -0,0 +1,23 @@
+
+
+//Time complexity: O(n*2^n)
+//space Complexity: O(1)
+//non recursive approach
+class Solution {
+   
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+
+        for(int i = 0;i<nums.length;i++){
+            int size = result.size();
+            for(int j = 0;j<size;j++){
+                List<Integer> li = new ArrayList<>(result.get(j));
+                li.add(nums[i]);
+                result.add(li);
+            }
+        }
+        return result;
+    }
+   
+}
